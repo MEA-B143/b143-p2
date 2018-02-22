7fb1c9f529b0ca8947ed4336fa589f78d2101ed6
Victor <vroua17@student.aau.dk>
2018-02-22 11:49:16 +0100

    Added the book class within the project folder

---
+++ src/P2/book.java
@@ -0,0 +1,11 @@
+package P2;
+
+public class book {
+		String title;
+		String author;
+		String genre;
+		String publication_date;
+		String publisher;
+		String return_date;
+		String lending_date;
+	}
