8a107f69ad3e49bf19b9f65948d624214315e369
NlGHT <NlGHT@users.noreply.github.com>
2018-02-27 12:01:17 +0100

    Updated User and Book

--- src/P2/Book.java
+++ src/P2/Book.java
@@ -1,11 +1,28 @@
 package P2;
-
+import java.util.*;
 public class Book {
-		String title;
-		String author;
-		String genre;
-		String publication_date;
-		String publisher;
-		String return_date;
-		String lending_date;
+	String title;
+	String author;
+	String genre;
+	Date publication_date;
+	String publisher;
+	Date return_date;
+	Date lending_date;
+	boolean borrowed;
+	int lendedUserID;
+	String lendedUserName;
+	
+	Book(String genre, String title, String author, String publisher) {
+		this.author = author;
+		this.title = title;
+		this.genre = genre;
+		this.publisher = publisher;
+		publication_date = new Date();
 	}
+	
+	public void borrow(User user) {
+		lendedUserID = user.id;
+		lendedUserName = user.name;
+		lending_date = new Date();
+	}
+}
--- src/P2/User.java
+++ src/P2/User.java
@@ -1,7 +1,7 @@
 package P2;
 
 public class User {
-	String user_id;
-	String user_name;
-	String user_password;
+	int id;
+	String name;
+	String password;
 }
