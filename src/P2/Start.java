dfe194755a3dc974821855b13c4625cd99f08a90
Victor <vroua17@student.aau.dk>
2018-03-01 09:24:08 +0100

    Changed the meta info of the books

--- src/P2/Start.java
+++ src/P2/Start.java
@@ -5,10 +5,10 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Book[] library = new Book[4];
-		library[0] = new Book("Crime fiction", "Finding bigfoot", "Morten", "Aalborg University");
-		library[1] = new Book("Science fiction", "Lord of the space ting", "Damiano", "Aalborg University");
-		library[2] = new Book("Pure fire", "Da ting goes mans not hot", "Big Shaq", "Island Records");
-		library[3] = new Book("Romantic Comedy", "You can do small kids", "Hendrik", "Aalborg University");
+		library[0] = new Book("Adventure", "Treasure Island", "Robert Louis Stevenson", "Penguin Books");
+		library[1] = new Book("Science fiction", "Stars Like Dust", "Isaac Asimov", "Penguin Books");
+		library[2] = new Book("History", "Shogun", "James Clavell", "Penguin Books");
+		library[3] = new Book("Fantasy", "The Metamorphosis", "Franz Kafka", "Penguin Books");
 		User bigShaq = new User("Big Shaq", "Mansnothot");
 
 		library[0].borrow(bigShaq);
